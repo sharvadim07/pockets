6e4315cae39f92623aa3679ad623d7341b2644cc
Vadim <sharvadim07@yandex.ru>
2023-11-06 13:22:23 +0700

    Add Simple JWT authentication

--- server/apps/users/urls/auth.py
+++ server/apps/users/urls/auth.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from rest_framework.authtoken import views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 from apps.users.views import UserCreateAPIView
 
+
 urlpatterns = [
     path('register/', UserCreateAPIView.as_view()),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('api-token-auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api-token-auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
