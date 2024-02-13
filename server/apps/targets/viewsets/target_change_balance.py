162dd74fe2adea13bfb1c08c1ef5358f4f8e1db3
Vadim <sharvadim07@yandex.ru>
2024-02-13 20:22:51 +0700

    Remove List mixin from TargetChangeBalanceViewSet

--- server/apps/targets/viewsets/target_change_balance.py
+++ server/apps/targets/viewsets/target_change_balance.py
@@ -1,34 +1,23 @@
-from apps.targets.models.querysets.target_change_balance import (
-    TargetChangeBalanceQuerySet,
-)
 from apps.targets.models.target_change_balance import TargetChangeBalance
 from apps.targets.serializers.target_change_balance import (
     TargetChangeBalanceCreateSerializer,
     TargetChangeBalanceRetrieveSerializer,
 )
-from rest_framework import pagination, viewsets
+from rest_framework import mixins, pagination, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 
-class TargetChangeBalanceViewSet(viewsets.ModelViewSet):
+class TargetChangeBalanceViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     pagination_class = pagination.LimitOffsetPagination
     pagination_class.default_limit = 20
     permission_classes = (IsAuthenticated,)
     queryset = TargetChangeBalance.objects.all()
-
-    def get_queryset(self) -> TargetChangeBalanceQuerySet:
-        return (
-            TargetChangeBalance.objects.filter(
-                user=self.request.user,
-            )
-            .select_related(
-                "category",
-            )
-            .order_by(
-                "-date",
-                "-id",
-            )
-        )
 
     def get_serializer_class(self):
         if self.action in {"create", "update", "partial_update"}:
